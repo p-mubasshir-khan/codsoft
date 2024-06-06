0cc2f0ac6913c81d3462f2ca5f78ed0b8cda96c5
p-mubasshir-khan <khan.srmap@gmail.com>
2024-06-06 15:49:02 +0530

    TASK 1 .py

--- TASK 1 .py
+++ TASK 1 .py
@@ -1,3 +1,8 @@
+#A To-Do List application is a useful project that helps users manage
+#and organize their tasks efficiently. This project aims to create a
+#command-line or GUI-based application using Python, allowing
+
+users to create, update, and track their to-do lists
 def print_tasks(task_list):
     task_list.sort(key=lambda x: -int(x[2]))
     for row in task_list:
